6f649890f1d034961234a13f2eb208b401727c37
Hans Hofner <hhofner@gmail.com>
2020-04-04 20:45:35 +0900

    add contact view cell

---
+++ LapizLazuli/View/ContactCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ContactCollectionViewCell.swift
+//  LapizLazuli
+//
+//  Created by Hans Hofner on 2020/04/04.
+//  Copyright © 2020 NaposuTech. All rights reserved.
+//
+
+import UIKit
+
+class ContactCollectionViewCell: UICollectionViewCell {
+
+    weak var textLabel: UILabel!
+    
+}
